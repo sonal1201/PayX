d36c06f82d3500ec798b00a67621b626feb8674f
Sonal Singh <sonal1201kumar@gmail.com>
2025-08-30 00:10:28 +0530

    Added: Account-model

---
+++ backend/src/models/account-model.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+})
+
+export const Account = mongoose.model('Account', accountSchema);
